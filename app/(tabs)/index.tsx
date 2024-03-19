723c99ac94d14316c0eb0086a32ea7996ecb5d8d
Hoang Hy <hoanghy01122003@gmail.com>
2024-03-19 10:34:28 +0700

    feat: add login page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,92 @@
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet, TextInput } from "react-native";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { Text, View } from "@/components/Themed";
+import { USERS } from "@/constants/defaultData";
+import { router } from "expo-router";
+import { useState } from "react";
 
 export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
-  );
+	const [username, setUsername] = useState<string>();
+	const [password, setPassword] = useState<string>();
+
+	const [message, setMessage] = useState<string>();
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>Login</Text>
+			<View style={styles.main}>
+				<Text style={styles.label}>Username</Text>
+				<TextInput
+					style={styles.input}
+					onChangeText={(text) => {
+						setUsername(text);
+						setMessage("");
+					}}
+					defaultValue={username}
+				/>
+				<Text style={styles.label}>Password</Text>
+				<TextInput
+					textContentType={"password"}
+					style={styles.input}
+					onChangeText={(text) => {
+						setPassword(text);
+						setMessage("");
+					}}
+					defaultValue={password}
+				/>
+			</View>
+			{message ? <Text style={styles.message}>{message}</Text> : null}
+			<Button
+				onPress={() => {
+					if (
+						USERS.some(
+							({ username: username_, password: password_ }) =>
+								username == username_ && password == password_
+						)
+					) {
+						router.replace(`/(tabs)/${username}`);
+					} else {
+						setMessage("Username or password is wrong");
+					}
+				}}
+				title="Sign in"
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+	container: {
+		flex: 1,
+		paddingTop: 50,
+		paddingHorizontal: 20,
+		gap: 50,
+		// alignItems: "center",
+		// justifyContent: "center",
+	},
+	title: {
+		fontSize: 32,
+		fontWeight: "bold",
+	},
+	main: {},
+	label: {
+		fontSize: 20,
+		marginBottom: 10,
+		fontWeight: "600",
+		marginTop: 30,
+	},
+	input: {
+		height: 40,
+		borderWidth: 1,
+		padding: 10,
+		fontSize: 20,
+		borderRadius: 5,
+	},
+	message: {
+		fontSize: 20,
+		color: "red",
+	},
+	button: {
+		textTransform: "capitalize",
+	},
 });
